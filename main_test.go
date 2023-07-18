1a1271a0c5e6a85a7b56ad620636892e9bff33cc
Alexadre de Almeida Lunkes <lunkes.alexandre@gmail.com>
2023-07-18 11:21:26 -0300

    test: :white_check_mark: add test to ci

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestIfSomaMakeSum(t *testing.T) {
-	total := soma(15, 15)
+	total := soma(14, 15)
 	if total != 30 {
 		t.Errorf("O Resultado é inválido: Resultado %d - Experado %d", total, 30)
 	}
