13293a40d55bcea95dcb09bbad6b37ec54960cd0
Alexadre de Almeida Lunkes <lunkes.alexandre@gmail.com>
2023-07-18 10:42:38 -0300

    test: :test_tube: add test to subtracao

--- main_test.go
+++ main_test.go
@@ -8,3 +8,10 @@ func TestIfSomaMakeSum(t *testing.T) {
 		t.Errorf("O Resultado é inválido: Resultado %d - Experado %d", total, 30)
 	}
 }
+
+func TestIfSubtracaoIsOk(t *testing.T) {
+	total := subtracao(15, 5)
+	if total != 10 {
+		t.Errorf("O Resultado é inválido: Resultado %d - Experado %d", total, 10)
+	}
+}
